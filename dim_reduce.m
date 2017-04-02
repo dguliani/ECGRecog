6cf73fd0011ee965e9dfb1431f8ec85c540c7514
Nestor Abarca <nbarcs@gmail.com>
2017-04-02 16:14:06 -0400

    Initial commit of dimension reduction func

---
+++ dim_reduce.m
@@ -0,0 +1,44 @@
+clear all
+clc
+
+% get the data from the dataset and split labels and samples
+dataset = csvread('features.csv');
+labels = dataset(:,1);
+featureset = dataset(:,2:size(dataset,2));
+featureset = featureset';
+
+%% Principle Component Analysis
+%http://ufldl.stanford.edu/wiki/index.php/Implementing_PCA/Whitening
+
+% find the average across the rows
+% this might be wrong, will need to investigate
+avg = mean(featureset,1);
+% zero the mean across the rows
+featureset = featureset - repmat(avg, size(featureset,1), 1);
+
+% find the standard deviation
+sigma = featureset * featureset' / size(featureset,2);
+
+% calculate the Single Value Decomp for the transformation
+[U,S,V] = svd(sigma);
+
+% rotate the matrix
+xRot = U' * featureset;
+
+% reduce the dimensionality to dimensionality k
+k = 2;
+xTilde = U(:,1:k)' * featureset;
+
+% get some colours for the 2D component scatter plot
+colour = labels/100;
+colours = [colour colour-colour/2 colour/colour];
+
+% transpose matrix to form a larger matrix
+newX = xTilde';
+% scatter plot
+% from this scatter plot, we need to fit in elliptoids like she did to
+% visualize. it still really doesn't look like hers though
+scatter(newX(:,1), newX(:,2), 20,[colours(:,1) colours(:,2) colours(:,3)]);
+
+
+
